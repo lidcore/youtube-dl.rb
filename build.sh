0cb009324dd518d9bb1e5fb0e03e9d6ab65192c5
Neil Mock <neilmock@gmail.com>
2019-09-06 22:58:23 +0000

    set executable

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ set -e
 docker build --no-cache --build-arg YOUTUBE_DL_VERSION=2019.09.01 -t youtubedl-heroku18 .
 
 docker run --rm -t -v $PWD/vendor/bin:/data youtubedl-heroku18 sh -c 'cp -f /usr/local/bin/youtube-dl /data/'
+
+chmod gou+x $PWD/vendor/bin/youtube-dl
